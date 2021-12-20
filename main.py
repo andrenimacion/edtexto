87c666e44ff5c21562e53c4824772b3e49f56f9f
Andre Rivera <v.humandreams@gmail.com>
2021-12-20 16:50:55 -0500

    Procesos terminados

--- main.py
+++ main.py
@@ -6,26 +6,29 @@ import json
 
 
 class Main_class:
-    def __init__(self, window):
+    def __init__(self, window, ruta):
         
-        # with open(src) as content:
-        #     res = json.load(content)
-        #     self.size = res.get("title")
-        #     self.title = res.get("size")
+        with open(ruta) as content:
+            res = json.load(content)
+            self.size  = res.get("size")
+            self.title = res.get("title")
 
         self.wind = window
-        self.wind.title("App search text")
-        self.wind.geometry("600x350")
-        
-        
+        self.wind.title(self.title)
+        self.wind.geometry(self.size)
+        labelLeft  = LabelFrame(self.wind, text="Ingresar texto:", pady=10, padx=15)
+        labelLeft.grid(row=1, column=0)
+        labelRight = LabelFrame(self.wind, text="Resultado del cambio:", pady=10, padx=15)
+        labelRight.grid(row=1, column=1)
         self.struc = App_structure()
-        self.struc.tex_area(self.wind)
+        self.struc.tex_area(labelLeft, labelRight)
+        labelHead  = Label(self.wind, text="Data edit aplicación: v.1.0.0.1", justify=LEFT)
+        labelHead.grid(row=3, column=0, columnspan=2)
 
 
 
 if __name__ == "__main__":
     window = Tk()
-    # xRuta = "config.py.json"
-    xCls  = Main_class(window)
-    
+    xRuta = "config_py.json"
+    xCls  = Main_class(window, xRuta)
     window.mainloop()
--- structure.py
+++ structure.py
@@ -1,30 +1,68 @@
 from tkinter import *
 from tkinter import ttk
+from typing import ValuesView
 
 
 class App_structure:
-    def tex_area(self, object):
-        self.lFrames = LabelFrame(object, text="Ingresar texto:")
+    def tex_area(self, objectA, objectB):
+        self.lFrames = LabelFrame(objectA, bg="#D6D6D6")
         self.lFrames.pack()
-        self.tarea = Text(self.lFrames, width=50, height=10)
-        self.tarea.pack()
-        self.sFrames = LabelFrame(object, text="Ingrese un caracter o palabra a buscar")
+        self.tareaA = Text(self.lFrames, width=50, height=10)
+        self.tareaA.pack()
+        self.sFrames = LabelFrame(objectA, text="Ingrese un caracter o palabra a buscar")
         self.sFrames.pack()
+        self.lab_char = Label(self.sFrames, bg="#D6D6D6", text="Caracter a buscar:")
+        self.lab_char.pack(side=LEFT)
+        self.inputB = Entry(self.sFrames)
+        self.inputB.pack(side=LEFT)
+        self.lab_char = Label(self.sFrames, bg="#D6D6D6", text="Caracter a reemplazar:")
+        self.lab_char.pack(side=LEFT)
         self.input = Entry(self.sFrames)
-        self.input.pack()
-        self.Btn = Button(self.sFrames, text="Buscar", command=self.GetData)
-        self.Btn.pack()
+        self.input.pack(side=LEFT)
+        self.Btn = Button(self.sFrames, text="Buscar", command=lambda: self.GetData(objectA))
+        self.Btn.pack(side=BOTTOM)
+        self.rightFrames = LabelFrame(objectB)
+        self.rightFrames.pack()
+        self.tareaB = Text(self.rightFrames, bg="#D6D6D6", fg="black", 
+                           width=50, height=14)
+        self.tareaB.insert("1.0", "Esperando data...")
+        self.tareaB.pack()
 
 
-
-    def GetData(self):
-        self.x_data  = self.input.get()
-        self.x_tarea = self.tarea.get("1.0", "end")
-
-        print(self.x_tarea.index(self.x_data))
-                
-        # self.labRes = Label(text=)
+    def GetData(self, objectA):
+        #caracter a reemplazar
+        self.x_data     = self.input.get()
+        #caracter a buscar para cambiar
+        self.changeData = self.inputB.get()
+        print(self.changeData)
+        self.x_tarea = self.tareaA.get("1.0", "end")
+        # m = self.x_tarea.index(self.x_data)
         
-        # print(self.x_tarea)
-        # print(self.x_data )
+        self.rep_cadena = self.x_tarea.replace(self.changeData, self.x_data)
+        print(self.rep_cadena)
+
+        self.tareaB.delete("1.0", END)
+        self.tareaB.insert("1.0", self.rep_cadena)
+        # self.x_tarea
+        
+        
+        self.c_data = str(self.Longitud(self.x_tarea))
+        self.labRes = Label(objectA, fg="green", bg="#D6D6D6",
+                            padx=5, pady=5, text= "C. Ingresados: " + self.c_data)  
+        
+        self.labRes.pack(side=RIGHT)
+        
+        # self.labRes = Label(self.sFrames, fg="green", bg="#D6D6D6",
+        #                     padx=5, pady=5, text= "Número de caracteres ingresados: " + self.c_data)  
+        
+        # self.labRes.pack()
+        
+        
+        
+        
+    def Longitud( self, data_analizar ):
+        x   = list( data_analizar )
+        s_x = len ( x )
+        return s_x
+
 
